06d85a4f56114423d28e1f1715bcecd33813483f
yurii <ukoval400@gmail.com>
2024-12-20 20:23:49 +0100

    first commit

---
+++ api-gateway/src/main/java/javabegin/micro/demo/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package javabegin.micro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
